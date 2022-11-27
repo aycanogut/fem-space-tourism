af03830ca6a9ee07e9d4ff5cc1b561dc6a6274e1
Aycan Ogut <aycanonfire@gmail.com>
2022-11-27 13:52:58 +0300

    feat: :sparkles: add application pages && define paths

---
+++ src/pages/technology/index.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+import Layout from '../../components/Layout/Layout'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Space Tourism | Technology</title>
+      </Head>
+
+      <Layout path="technology" />
+    </>
+  )
+}
+
+export default Home
