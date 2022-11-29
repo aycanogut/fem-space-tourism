6f45acd937424c25387fe825cd82a11ce2146b53
Aycan Ogut <aycanonfire@gmail.com>
2022-11-29 18:12:36 +0300

    feat: :sparkles: add button to homepage && update tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from 'next'
 import Head from 'next/head'
 
 import Layout from '../components/Layout'
+import Button from '../components/Button'
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +11,12 @@ const Home: NextPage = () => {
         <title>Space Tourism</title>
       </Head>
 
-      <Layout path="home" />
+      <Layout path="home">
+        <Button
+          text="explore"
+          className="bold text- hover: b mt-56 h-button_sm w-button_sm rounded-full bg-white font-bellefair text-button font-normal uppercase tracking-tight text-vulcan transition-all hover:shadow-button md:h-button_md md:w-button_md md:text-heading_4 md:tracking-normal lg:h-button_lg lg:w-button_lg"
+        />
+      </Layout>
     </>
   )
 }
