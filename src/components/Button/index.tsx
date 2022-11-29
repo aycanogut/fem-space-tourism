58b4c3e133f245dbf8e936badd763fbf1633df67
Aycan Ogut <aycanonfire@gmail.com>
2022-11-29 18:22:08 +0300

    feat: :sparkles: update button as next link

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react'
+import Link from 'next/link'
 
 import { IButtonProps } from '../../interfaces/Button.interface'
 
 const Button: FC<IButtonProps> = ({ text }) => {
   return (
-    <button className="bold text- hover: b mt-56 h-button_sm w-button_sm rounded-full bg-white font-bellefair text-button font-normal uppercase tracking-tight text-vulcan transition-all hover:shadow-button md:h-button_md md:w-button_md md:text-heading_4 md:tracking-normal lg:h-button_lg lg:w-button_lg ">
-      {text}
-    </button>
+    <Link passHref href="/destination">
+      <a>
+        <button className="bold text- hover: b mt-56 h-button_sm w-button_sm rounded-full bg-white font-bellefair text-button font-normal uppercase tracking-tight text-vulcan transition-all hover:shadow-button md:h-button_md md:w-button_md md:text-heading_4 md:tracking-normal lg:h-button_lg lg:w-button_lg ">
+          {text}
+        </button>
+      </a>
+    </Link>
   )
 }
 
