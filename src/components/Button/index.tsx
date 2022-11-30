ba8e7dc20a116d89aaabdd8b0bc97406c7eb063f
Aycan Ogut <aycanonfire@gmail.com>
2022-11-30 18:15:50 +0300

    refactor: :lipstick: update design tokens name convention

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,7 +7,7 @@ const Button: FC<IButtonProps> = ({ text }) => {
   return (
     <Link passHref href="/destination">
       <a>
-        <button className="bold text- hover: b mt-56 h-button_sm w-button_sm rounded-full bg-white font-bellefair text-button font-normal uppercase tracking-tight text-vulcan transition-all hover:shadow-button md:h-button_md md:w-button_md md:text-heading_4 md:tracking-normal lg:h-button_lg lg:w-button_lg ">
+        <button className="text-bold h-button_sm w-button_sm rounded-full bg-white font-bellefair text-20 font-normal uppercase tracking-tighter text-vulcan transition-all hover:shadow-button md:h-button_md md:w-button_md md:text-32 md:tracking-tight lg:h-button_lg lg:w-button_lg">
           {text}
         </button>
       </a>
