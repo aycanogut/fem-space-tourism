41db69ac276cfc736893ca9d834c81568f059534
Aycan Ogut <aycanonfire@gmail.com>
2022-12-10 15:28:16 +0300

    feat: :lipstick: update styling && add link and target

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,9 +5,9 @@ import { IButtonProps } from '../../interfaces/Button.interface'
 
 const Button: FC<IButtonProps> = ({ text }) => {
   return (
-    <Link passHref href="/destination">
-      <a>
-        <button className="text-bold h-button_sm w-button_sm rounded-full bg-white font-bellefair text-20 font-normal uppercase tracking-tighter text-vulcan transition-all hover:shadow-button md:h-button_md md:w-button_md md:text-32 md:tracking-tight lg:h-button_lg lg:w-button_lg">
+    <Link passHref href="https://aycan.dev">
+      <a target="_blank">
+        <button className="text-bold mt-20 h-button_sm w-button_sm rounded-full bg-white font-bellefair text-20 font-normal uppercase tracking-tighter text-vulcan transition-all hover:shadow-button md:mt-[158px] md:h-button_md md:w-button_md md:text-32 md:tracking-tight lg:mr-2 lg:mt-[395px] lg:h-button_lg lg:w-button_lg">
           {text}
         </button>
       </a>
