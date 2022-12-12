e0f1ec1e9a6844d4df8adaf9f206bfffc73bf161
Aycan Ogut <aycanonfire@gmail.com>
2022-12-12 17:40:46 +0300

    refactor: :alien: add swr and update content data as props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
+import useSWR from 'swr'
+
+import fetcher from '../helpers/fetcher'
 
 import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import Button from '../components/Button'
 
 const Home: NextPage = () => {
+  const { data } = useSWR('/json/data.json', fetcher)
+
   return (
     <>
       <Head>
@@ -16,8 +21,12 @@ const Home: NextPage = () => {
         path="home"
         className="flex-col items-center lg:flex-row lg:justify-between lg:px-40"
       >
-        <Hero />
-        <Button text="explore" />
+        <Hero
+          uppertitle={data?.homepage?.uppertitle}
+          title={data?.homepage?.title}
+          description={data?.homepage?.description}
+        />
+        <Button text={data?.homepage?.button} />
       </Layout>
     </>
   )
