d9d2a52a0dfb8381f46c6ad14eb62310905719f0
Aycan Ogut <aycanonfire@gmail.com>
2022-12-13 22:46:03 +0300

    fix: :lipstick: fix spacing on tablet screens

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,10 +5,10 @@ import { IButtonProps } from '../../interfaces/Button.interface'
 
 const Button: FC<IButtonProps> = ({ text }) => {
   return (
-    <div className="pt-20 md:pt-[158px] lg:pt-[395px]">
+    <div className="pt-20 md:pt-[148px] lg:pt-[395px]">
       <Link passHref href="https://aycan.dev">
         <a target="_blank">
-          <button className="text-bold h-button_sm w-button_sm rounded-full bg-white font-bellefair text-20 font-normal uppercase tracking-tighter text-vulcan transition-all hover:shadow-button  md:h-button_md md:w-button_md md:text-32 md:tracking-tight lg:mr-2  lg:h-button_lg lg:w-button_lg">
+          <button className="text-bold h-button_sm w-button_sm rounded-full bg-white font-bellefair text-20 font-normal uppercase tracking-tighter text-vulcan transition-all hover:shadow-button md:h-button_md md:w-button_md md:text-32 md:tracking-tight lg:mr-2 lg:h-button_lg lg:w-button_lg">
             {text}
           </button>
         </a>
